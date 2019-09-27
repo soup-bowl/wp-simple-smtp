7702ab7c73d4b2f5b4469033102c9a42c59fa9eb
Casey <code@soupbowl.io>
2019-09-27 14:30:10 +0000

    Update docker-compose.yml, plugin.php files

---
+++ plugin.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Adds mail configuration to WordPress in a simple, standardised plugin.
+ *
+ * @package sb-simple-smtp
+ * @author soup-bowl
+ * @license MIT
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Simple SMTP
+ * Description:       Adds mail configuration to WordPress in a simple, standardised plugin.
+ * Plugin URI:        https://gitlab.com/soup-bowl/simple-smtp
+ * Version:           trunk
+ * Author:            soup-bowl
+ * Author URI:        https://soupbowl.io
+ * License:           MIT
+ */
+
+ // TODO
+ //phpinfo();
