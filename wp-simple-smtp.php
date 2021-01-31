632cd8bf761345f4b3eccf4c3f27c7ab79dcc550
soup-bowl <code@soupbowl.io>
2021-01-31 02:27:46 +0000

    Tracker is bumped to minor release due to #29.

--- wp-simple-smtp.php
+++ wp-simple-smtp.php
@@ -10,7 +10,7 @@
  * Plugin Name:       Simple SMTP
  * Description:       Adds mail configuration to WordPress in a simple, standardised plugin.
  * Plugin URI:        https://www.soupbowl.io/wp-plugins
- * Version:           1.0.2
+ * Version:           1.1
  * Author:            soup-bowl
  * Author URI:        https://www.soupbowl.io
  * License:           MIT
