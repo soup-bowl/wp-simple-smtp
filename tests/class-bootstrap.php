0b23d36c0b0577726067ffb560317d945455ff95
soup-bowl <code@soupbowl.io>
2021-02-19 14:48:01 +0000

    Partial testing of logservice.

--- tests/class-bootstrap.php
+++ tests/class-bootstrap.php
@@ -19,10 +19,10 @@ $GLOBALS['sbss_temp_store'] = [];
  * Mocks the WordPress add_action function.
  * See https://developer.wordpress.org/reference/functions/add_action/ for more information.
  *
- * @param string   $tag             Unused.
- * @param mixed    $function_to_add Unused.
- * @param integer  $priority        Unused.
- * @param integer  $accepted_args   Unused.
+ * @param string  $tag             Unused.
+ * @param mixed   $function_to_add Unused.
+ * @param integer $priority        Unused.
+ * @param integer $accepted_args   Unused.
  * @return true
  */
 function add_action( $tag, $function_to_add, $priority = 10, $accepted_args = 1 ) {
@@ -33,10 +33,10 @@ function add_action( $tag, $function_to_add, $priority = 10, $accepted_args = 1 
  * Mocks the WordPress add_filter function.
  * See https://developer.wordpress.org/reference/functions/add_filter/ for more information.
  *
- * @param string   $tag             Unused.
- * @param mixed    $function_to_add Unused.
- * @param integer  $priority        Unused.
- * @param integer  $accepted_args   Unused.
+ * @param string  $tag             Unused.
+ * @param mixed   $function_to_add Unused.
+ * @param integer $priority        Unused.
+ * @param integer $accepted_args   Unused.
  * @return true
  */
 function add_filter( $tag, $function_to_add, $priority = 10, $accepted_args = 1 ) {
