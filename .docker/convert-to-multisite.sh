e2e52f80d8238f1219888657a96829068fbaf722
soup-bowl <code@soupbowl.io>
2021-09-12 13:38:23 +0100

    Multisite conversion script expanded as quickstart

--- .docker/convert-to-multisite.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-echo "Adding multisite defines and replacing htaccess..."
-# Swap htaccess for a MS one - https://wordpress.org/support/article/htaccess/
-rm /var/www/html/.htaccess && cp /opt/wpss/htaccess /var/www/html/.htaccess
-# Modify the wp-config.php to contain multisite configurations.
-wp core multisite-install --title="abc" --admin_email="code@soupbowl.io" --allow-root
-echo "Multisite modifications made. You can re-login now."
