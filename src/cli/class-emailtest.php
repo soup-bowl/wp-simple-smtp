98b115222071e17db7ddf4f99f54a96564c6aacd
soup-bowl <code@soupbowl.io>
2021-11-07 23:34:22 +0000

    (CLI) Allow plain/html override.

--- src/cli/class-emailtest.php
+++ src/cli/class-emailtest.php
@@ -11,6 +11,7 @@ namespace wpsimplesmtp\cli;
 
 use wpsimplesmtp\Mailtest;
 use WP_CLI;
+use WP_CLI\Utils;
 
 /**
  * Adds operations to WP-CLI environments.
@@ -21,6 +22,9 @@ class EmailTest {
 	 *
 	 * <email>
 	 * : Email address to send the test to.
+	 * 
+	 * [--html]
+	 * : Sends a HTML-based email instead of plaintext.
 	 *
 	 * @when before_wp_load
 	 *
@@ -28,8 +32,10 @@ class EmailTest {
 	 * @param array $assoc_args Associated arguments.
 	 */
 	public function test_email( $args, $assoc_args ) {
+		$is_html = Utils\get_flag_value( $assoc_args, 'html' );
+
 		if ( is_email( $args[0] ) ) {
-			$email     = Mailtest::generate_test_email( true );
+			$email     = Mailtest::generate_test_email( ( $is_html ) ? true : false );
 			$recipient = sanitize_email( $args[0] );
 
 			$is_sent = wp_mail( $recipient, $email['subject'], $email['message'], $email['headers'] );
